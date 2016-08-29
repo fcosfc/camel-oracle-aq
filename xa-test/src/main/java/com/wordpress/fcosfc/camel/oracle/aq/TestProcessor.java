f44d1227acc5892e2bef2d2658b66c6d35de884a
Paco Saucedo <fcosaucedo@gmail.com>
2016-08-29 19:02:58 +0200

    Adjustments on the XA test

---
+++ xa-test/src/main/java/com/wordpress/fcosfc/camel/oracle/aq/TestProcessor.java
@@ -0,0 +1,63 @@
+package com.wordpress.fcosfc.camel.oracle.aq;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Dummy Camel Processor that simulates msg processing. Increments a static
+ * counter for each msgs received.
+ *
+ * Can also be used to raise an exception for every X msg to illustrate how an
+ * exception causes the transaction to be rolled back.
+ *
+ */
+public class TestProcessor implements Processor {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
+
+    private boolean simulateProcessingError = false;
+
+    private int errorAfterMsgs = 5;
+
+    private Logger log = LoggerFactory.getLogger(TestProcessor.class);
+
+    public void process(Exchange exchange) throws Exception {
+
+        if (log.isTraceEnabled()) {
+            log.trace("Starting to process messages.");
+        }
+
+        counter.incrementAndGet();
+
+        if (simulateProcessingError) {
+            if (counter.get() % errorAfterMsgs == 0) {
+                throw new RuntimeException("Thrown from Camel Processor to simulate an exception in Camel route");
+            }
+        }
+
+        if (log.isTraceEnabled()) {
+            log.trace("Finished msg processing. Counter is at " + counter.get());
+        }
+    }
+
+    public boolean isSimulateProcessingError() {
+        return simulateProcessingError;
+    }
+
+    public void setSimulateProcessingError(boolean simulateProcessingError) {
+        this.simulateProcessingError = simulateProcessingError;
+    }
+
+    public int getErrorAfterMsgs() {
+        return errorAfterMsgs;
+    }
+
+    public void setErrorAfterMsgs(int errorAfterMsgs) {
+        this.errorAfterMsgs = errorAfterMsgs;
+    }
+}
